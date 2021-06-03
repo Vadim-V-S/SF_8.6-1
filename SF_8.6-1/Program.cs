a06ab9276a26050e38115c8f56ba4463e93ae5d2
Vadim-V-S <vaddimsav@gmail.com>
2021-06-03 12:22:40 +0500

    Добавьте файлы проекта.

---
+++ SF_8.6-1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+
+namespace SF_8._6_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите путь до папки:\t");
+            string path = string.Format(@"{0}", Console.ReadLine());
+            DeleteFiles(path);
+
+            Console.ReadKey();
+        }
+        static void DeleteFiles(string path)
+        {
+            try
+            {
+                DirectoryInfo dir = new DirectoryInfo(path);
+                if (dir.Exists)
+                {
+                    foreach (var file in Directory.GetFiles(path))
+                    {
+                        DateTime fileCreationTime = File.GetCreationTime(file);
+                        TimeSpan t = DateTime.Now - fileCreationTime;
+                        if (t > TimeSpan.FromMinutes(30))
+                        {
+                            File.Delete(file);
+                            Console.WriteLine("{0} - удален", file);
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
